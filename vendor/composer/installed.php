9c73180d3446c09ece891553afba7132d6d2aca5
SAKURA1732 <ymdakk8490@gmail.com>
2024-11-20 23:44:09 +0900

    add files

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '78415ae99c2840ae80fdf086ca48ab8483edb00b',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -389,9 +389,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '78415ae99c2840ae80fdf086ca48ab8483edb00b',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
